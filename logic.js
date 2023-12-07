2ff034aa7125d8a7cedb029bbf8d4a41d3c4400c
Th3Mayar <josehenriquez.02.26@gmail.com>
2023-12-07 12:25:29 -0400

    Update fix

--- logic.js
+++ logic.js
@@ -107,7 +107,7 @@ async function obtenerProductosUsuario() {
 async function logic() {
   while (true) {
     await obtenerProductosUsuario();
-    await delay(10000);
+    await delay(86400000);
   }
 }
 
